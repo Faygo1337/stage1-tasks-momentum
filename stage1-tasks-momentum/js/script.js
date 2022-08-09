717cd707ac766200c0cf8676afee65da13f67c75
Faygo1337 <maximgustynov@gmail.com>
2022-08-10 00:03:16 +0300

    feat: added alert in case of incorrect input in widget weather

--- stage1-tasks-momentum/js/script.js
+++ stage1-tasks-momentum/js/script.js
@@ -179,7 +179,8 @@ const humidity = document.querySelector('.humidity')
 const city = document.querySelector('.city')
 const cityOfMinsk = city.value = 'Minsk'
 async function getWeather() { 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=9c95a9e4172b94186fe814eb375797ef&units=metric`;
+    try {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=9c95a9e4172b94186fe814eb375797ef&units=metric`;
     const res = await fetch(url);   
     const data = await res.json();
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
@@ -188,19 +189,12 @@ async function getWeather() {
     wind.textContent = `Wind speed: ${Math.ceil(data.wind.speed)} m/s`;
     humidity.textContent = `Humidity: ${data.main.humidity}%`;
   }
+  catch(err){
+    alert(`Error! city not found for '${city.value}'!`)
+  };
+}
   getWeather()
-
-
   city.addEventListener('change', getWeather);
-
-
-
-
-
-
-
-
-
 
 
 
